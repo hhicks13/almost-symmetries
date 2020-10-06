c9c2c449d85b5bca79f570b27c32cc5c36f5bc01
Harrison Hicks <hhicks13@vols.utk.edu>
2020-10-05 20:44:06 -0400

    multidimensional scaling code being debugged

--- orbifold.py
+++ orbifold.py
@@ -303,7 +303,7 @@ def main():
     v = int(sys.argv[4])
     ###################################################################### Random Graph #
     
-    k = 5
+    Tau = int(sys.argv[5])
     P0 = []
     memo1 = {} 
     memo2 = {}
@@ -312,10 +312,11 @@ def main():
     ######################################################################## Generate P #
     for u,v in it.product(range(n),range(n)):
         num2namepoint,Nu,Nv,dim = genMunkresBasis(names,n,g,u,v)
+        #flow
         B,M  = hungarianSolve(g,num2namepoint,Nu,Nv,dim)
         #print(M)
         #print(nx.is_perfect_matching(B,M))
-        linsum = 0
+        linsum = extractweight(g,u,v)
         for m in M:linsum+=extractweight(g,m[0][1],m[1][1])
         #print(linsum)
         P0.append((u,v,linsum))
@@ -329,25 +330,125 @@ def main():
 
     ############################################# Refinement Algorithm 2: JimElim #######
     Rho[k+1] = P1
-    for t in range(k):
-        budget = k - t
-        print(budget)
-        Rho[budget] = [ (e[0],e[1],-1) if e[2]  > 2*budget else e for e in Rho[budget+1] ]
+    Rho2 = {}
+    memo = {}
+    casualties = 0
+    t = 0
+    # change budget to survivors 
+    while casualties < n*(n-1):
+        
+        # make permutation graph
+        epsilon = Tau - t
+        Rho[epsilon] = [ (e[0],e[1],-1) if e[2]  > 2*epsilon else e for e in Rho[epsilon+1] ]
+        # zero matrix 
+        delta = [(e[0],e[1],0) for e in Rho[epsilon]]
+        #for e in Rho[budget]:print(e)
+        
+        #pairs memoized
+        for e in Rho[epsilon]:
+            if e[2] == -1:
+                for i in range(n):
+                    delta[i*n+e[1]] = (e[0],e[1],e[2]+2)
+                    delta[e[0]*n+i] = (e[0],e[1],e[2]+2)
+                memo[(e[0],e[1])] = delta
+        #check to make sure copy works
+        
+        casualties = len(memo.keys())
+        print(casualties)
+        t+=1
     del Rho[k+1]
 
 
     ############################################# Algorithm 3 ###########################
+    # how does the graph change if you remove an edge.
+    #
+    # R = BinaryCopy(Rho[epsilon])
+    #
+    # w_ij = 1 - |Ni intersect Nj |/| Ni union Nj | <--- distance from center
+    # 
+    # Distance Matrix
+    #
+    # Apply multidimensional scaling
+    #
+    #
+    ############################################# matplot lib ############################
 
+    
 
-    ############################################# Print Loop ############################
-    sleeptime = 0.5
-    i = 0
-    while(i != -1):
-        i = (i+1)%(n)
-        for t in range(k):
-            budget = k-t
-            permutationTable(Rho[budget],n,n,0,budget)
-        time.sleep(sleeptime)
+    from matplotlib import pyplot as plt
+    from matplotlib.collections import LineCollection
+
+    from sklearn import manifold
+    from sklearn.metrics import euclidean_distances
+    from sklearn.decomposition import PCA
+
+    EPSILON = np.finfo(np.float32).eps
+    n_samples = 20
+    seed = np.random.RandomState(seed=3)
+    X_true = seed.randint(0, 20, 2 * n_samples).astype(np.float)
+    X_true = X_true.reshape((n_samples, 2))
+    # Center the data
+    X_true -= X_true.mean()
+
+    similarities = euclidean_distances(X_true)
+
+    # Add noise to the similarities
+    noise = np.random.rand(n_samples, n_samples)
+    noise = noise + noise.T
+    noise[np.arange(noise.shape[0]), np.arange(noise.shape[0])] = 0
+    similarities += noise
+
+    mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed,
+                   dissimilarity="precomputed", n_jobs=1)
+    pos = mds.fit(similarities).embedding_
+
+    nmds = manifold.MDS(n_components=2, metric=False, max_iter=3000, eps=1e-12,
+                    dissimilarity="precomputed", random_state=seed, n_jobs=1,
+                    n_init=1)
+    npos = nmds.fit_transform(similarities, init=pos)
+
+    # Rescale the data
+    pos *= np.sqrt((X_true ** 2).sum()) / np.sqrt((pos ** 2).sum())
+    npos *= np.sqrt((X_true ** 2).sum()) / np.sqrt((npos ** 2).sum())
+
+    # Rotate the data
+    clf = PCA(n_components=2)
+    X_true = clf.fit_transform(X_true)
+
+    pos = clf.fit_transform(pos)
+
+    npos = clf.fit_transform(npos)
+
+    fig = plt.figure(1)
+ax = plt.axes([0., 0., 1., 1.])
+
+s = 100
+plt.scatter(X_true[:, 0], X_true[:, 1], color='navy', s=s, lw=0,
+            label='True Position')
+plt.scatter(pos[:, 0], pos[:, 1], color='turquoise', s=s, lw=0, label='MDS')
+plt.scatter(npos[:, 0], npos[:, 1], color='darkorange', s=s, lw=0, label='NMDS')
+plt.legend(scatterpoints=1, loc='best', shadow=False)
+
+similarities = similarities.max() / (similarities + EPSILON) * 100
+np.fill_diagonal(similarities, 0)
+# Plot the edges
+start_idx, end_idx = np.where(pos)
+# a sequence of (*line0*, *line1*, *line2*), where::
+#            linen = (x0, y0), (x1, y1), ... (xm, ym)
+segments = [[X_true[i, :], X_true[j, :]]
+            for i in range(len(pos)) for j in range(len(pos))]
+values = np.abs(similarities)
+lc = LineCollection(segments,
+                    zorder=0, cmap=plt.cm.Blues,
+                    norm=plt.Normalize(0, values.max()))
+lc.set_array(similarities.flatten())
+lc.set_linewidths(np.full(len(segments), 0.5))
+ax.add_collection(lc)
+
+plt.show()
+    
+    
+        
 
 
 if __name__ == "__main__":
@@ -355,14 +456,4 @@ if __name__ == "__main__":
 #
 #
 #
-# check for memo collisions
-# properly compute the weight sum
-# find the iterator over all uv permutations which is appropriate.
-# debug memoloop
-# memoloop pass memo1/memo2 to orbif
-#
-# compute orbif:
-# (a,b,f(ab)) with f-table gathered from memoloop
-# plot orbif
-
 # print neuron-cosets based on memo2
